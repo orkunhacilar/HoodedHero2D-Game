b01d192660463f3cb99dffd3835b990f6052cb82
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-10-10 09:18:53 +0300

    14

--- HoodedHero2D/Assets/Scripts/Camera/CameraController.cs
+++ HoodedHero2D/Assets/Scripts/Camera/CameraController.cs
@@ -6,16 +6,38 @@ public class CameraController : MonoBehaviour
 {
     PlayerHaraketController player;
 
+    [SerializeField]
+    Collider2D boundsBox;
+
+    float halfYukseklik, halfGenislik;
+
+
+
     private void Awake()
     {
         player = Object.FindObjectOfType<PlayerHaraketController>();
+    }
+
+    private void Start()
+    {
+        halfYukseklik = Camera.main.orthographicSize; //kameranin ortasindan yukardaki uc sınırına olan uzaklık
+
+        halfGenislik = halfYukseklik * Camera.main.aspect;
+
+        print(halfYukseklik);
     }
 
     private void Update()
     {
         if (player != null)
         {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z); //Kamera Karakteri izlesin.
+            transform.position = new Vector3(
+                Mathf.Clamp(player.transform.position.x,boundsBox.bounds.min.x+halfGenislik,boundsBox.bounds.max.x-halfGenislik), //Clamp Sınırlamalar için kullandığımız fonksyon
+                Mathf.Clamp(player.transform.position.y,boundsBox.bounds.min.y+halfYukseklik,boundsBox.bounds.max.y-halfYukseklik),
+                transform.position.z); //Kamera Karakteri izlesin.
+
         }
     }
+
+    
 }
