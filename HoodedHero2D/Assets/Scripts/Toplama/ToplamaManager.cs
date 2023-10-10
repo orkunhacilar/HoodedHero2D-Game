03ef4a32f9b6aacb6096ce461db916c8de6ac859
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-10-10 10:34:01 +0300

    17.1

---
+++ HoodedHero2D/Assets/Scripts/Toplama/ToplamaManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToplamaManager : MonoBehaviour
+{
+
+    [SerializeField]
+    bool coinmi;
+
+    bool toplandimi;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !toplandimi)
+        {
+            toplandimi = true;
+            Destroy(gameObject);
+        }
+    }
+
+}
