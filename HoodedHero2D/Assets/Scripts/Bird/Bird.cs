d768d878ea8eeed9143f03c457d0765e42060ba9
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-10-10 21:14:49 +0300

    29

--- HoodedHero2D/Assets/Scripts/Bird/Bird.cs
+++ HoodedHero2D/Assets/Scripts/Bird/Bird.cs
@@ -8,14 +8,25 @@ public class Bird : MonoBehaviour
     Transform[] pozisyonlar;
 
     public float birdSpeed;
+    
 
     public float beklemeSuresi;
     float beklemeSayac;
+    
 
     int kacinciPozisyon;
 
+    Animator anim;
+
+    Vector2 kusYonu;
+    
+
+    Transform myTransform;
+
     private void Awake()
     {
+
+        anim = GetComponent<Animator>();
         foreach(Transform pos in pozisyonlar)
         {
             pos.parent = null;
@@ -24,6 +35,10 @@ public class Bird : MonoBehaviour
 
     private void Start()
     {
+        myTransform = GetComponent<Transform>();
+        Vector3 Birdscale = myTransform.localScale;
+        
+
         kacinciPozisyon = 0;
 
         transform.position = pozisyonlar[kacinciPozisyon].position;
@@ -31,14 +46,36 @@ public class Bird : MonoBehaviour
 
     private void Update()
     {
+       
+
         if (beklemeSayac > 0)
         {
             beklemeSayac -= Time.deltaTime; // geriye dogru say
+            anim.SetBool("UcsunMu", false);
 
         }
         else
         {
+            kusYonu = new Vector2(pozisyonlar[kacinciPozisyon].position.x - transform.position.x,
+               pozisyonlar[kacinciPozisyon].position.y - transform.position.y);
+
+            float angle = Mathf.Atan2(kusYonu.y, kusYonu.x)*Mathf.Rad2Deg;
+            // print(angle);
+
+            if (transform.position.x > pozisyonlar[kacinciPozisyon].position.x)
+            {
+                transform.localScale = new Vector3(1, -1, 1);
+            }
+            else
+            {
+                transform.localScale = Vector3.one;
+            }
+
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
             transform.position = Vector3.MoveTowards(transform.position, pozisyonlar[kacinciPozisyon].position, birdSpeed * Time.deltaTime);
+
+            anim.SetBool("UcsunMu", true);
 
             if (Vector3.Distance(transform.position, pozisyonlar[kacinciPozisyon].position) < 0.1f)
             {
